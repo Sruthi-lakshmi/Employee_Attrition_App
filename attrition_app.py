258f38e86af6af7fea4360918c41c7d5e1b6e6b1
Sruthi-lakshmi <sruthilakshmiranjith@gmail.com>
2024-03-29 11:47:29 +0530

    python file

--- attrition_app.py
+++ attrition_app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
-from dash import Dash, dcc, html, dash_table, callback, Input, Output
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, dash_table, callback, Input, Output, State
 
 df = pd.read_csv("employee_attrition.csv")
 
@@ -38,28 +39,59 @@ def barchart(table,table_name,categories):
 
 variables = df.columns.tolist()
 
-app = Dash(__name__)
+app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([html.Div(dcc.RadioItems(id='radio',options= ['Univariate','Bivariate'],value='Bivariate',labelStyle={"display": "inline-block"})),
-                    html.Div(dcc.Dropdown(id='dropdown',options=[{'label': i, 'value': i} for i in variables],value = "Age_Group",clearable=True)),
-                    html.Div(id='output')])
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.Img(src="static\image.png",alt="Logo",style={'width':'50%','display': 'block', 'margin': 'auto','vertical-align': 'middle'})
+        ],width={'size': 4}),
+
+        dbc.Col([html.H1("Employee Attrition Analysis Report",className='mb-2')])
+        ],align='center'),
+
+    dbc.Row([
+        dbc.Col([
+            html.Label('Click the Radio Buttons',style={'font':25,'fontWeight': 'normal','text-align': 'center'}),
+            dcc.RadioItems(id='radio',options= ['Univariate','Bivariate'],value='Bivariate', style={'color': 'Black', 'font-size': 25,'gap':'20x','padding-left': 10,'text-align': 'center'},
+                            labelStyle={"display": "inline-block", "align-items": "center"},className='me-2 mb-2 border border-grey')],
+            width={'size': 4,'offset':1},align='center'),
+            
+        dbc.Col([
+            html.Label('Select from the dropdown list',style={'font':25,'fontWeight': 'normal','color':'Black'}),
+            dcc.Dropdown(id='dropdown',options=[{'label': i, 'value': i} for i in variables],value = "Age_Group",searchable=True,clearable=True,placeholder="Please Select...",style={'width':'100%','color': 'Black'},className='mb-2'),
+            dbc.Button('Apply',color='primary',id ='button',outline=True,className='mb-4')],width={'size':6}),
+            dbc.Spinner(html.Div(id="loading-output"))]),
+
+    dbc.Row([
+        dbc.Col([
+            html.Div(id="button-updated"),
+            html.Div(id='output', children='Table and Chart')
+        ])
+    ])
+])
+
 @app.callback(
     Output(component_id='output', component_property='children'),
-    [Input(component_id='radio', component_property='value'),
-     Input(component_id='dropdown', component_property='value')]
+    [Input(component_id='button', component_property='n_clicks')],
+    [State(component_id='radio', component_property='value'),
+     State(component_id='dropdown', component_property='value')]
 )
-def update_output(radio_selected, selected_variable):
+def update_output(n_clicks, radio_selected, selected_variable):
+    if n_clicks is None:
+        return ''
+    html.Div([html.Label(f"Table and Chart {selected_variable}")])
     table = variable_table(df, selected_variable, 'Attrition')
-
     table = table[table[selected_variable] != "All"]
     categories = table[selected_variable].tolist()
-
+   
     if radio_selected == "Bivariate":
         bivariate = stacked_bar_chart(table, selected_variable, categories)
         output = html.Div([
             dash_table.DataTable(
                 data=table.to_dict('records'),
-                columns=[{"name": i, "id": i} for i in table.columns], id='table'),
+                columns=[{"name": i, "id": i} for i in table.columns], id='table',style_cell={'padding': '5px'},
+                style_header={'backgroundColor': '#C4E4FF','fontWeight': 'bold'}),
             dcc.Graph(id='barchart', figure=bivariate)])
     else:
         if radio_selected == "Univariate":
@@ -67,9 +99,11 @@ def update_output(radio_selected, selected_variable):
             output = html.Div([
                 dash_table.DataTable(
                     data=table.to_dict('records'),
-                    columns=[{"name": i, "id": i} for i in table.columns], id='table'),
+                    columns=[{"name": i, "id": i} for i in table.columns], id='table',
+                    style_header={'backgroundColor': '#C4E4FF','fontWeight': 'bold','border': '1px black'},
+                    style_cell={'padding': '5px'}),
                 dcc.Graph(id='barchart', figure=univariate)])
     return output
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,port=8051)
